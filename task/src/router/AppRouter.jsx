7311d49707b6d1481d3a4163881572db21bc6b11
Kamala <kamush9@gmail.com>
2023-12-11 19:42:09 +0400

    about added

--- task/src/router/AppRouter.jsx
+++ task/src/router/AppRouter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import News from "../pages/News";
-// import About from '../pages/About'
+import About from '../pages/About'
 
 const AppRouter = () => {
   return (
@@ -10,7 +10,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/news" element={<News />} />
-        {/* <Route path='/about' element={<About />}/>  */}
+        <Route path='/about' element={<About />}/> 
       </Routes>
     </div>
   );
